82ce11d550af21376d6a8a6c5861ebd191eea5dd
David <david.sanchez@tecnologicoliceocristiano.edu.ec>
2023-11-07 20:54:42 -0500

    Clase 7_11_2023

--- Clase_6_11_2023/MainPage.xaml.cs
+++ Clase_6_11_2023/MainPage.xaml.cs
@@ -11,11 +11,35 @@
             MiEstudiante = new Estudiantes();
             BindingContext = MiEstudiante;
         }
-
+        private bool IsValid()
+        {
+            if(string.IsNullOrEmpty(MiEstudiante.Nombre))
+            {
+                DisplayAlert("Registro Fallido", "Debe de ingresar un Nombre", "ok..");
+                return false;
+            }
+            if (string.IsNullOrEmpty(MiEstudiante.Apellido))
+            {
+                DisplayAlert("Registro Fallido", "Debe de ingresar un Apellido", "ok..");
+                return false;
+            }
+            if (string.IsNullOrEmpty(MiEstudiante.Materia))
+            {
+                DisplayAlert("Registro Fallido", "Debe ingresar el nombre de la materia", "ok..");
+                return false;
+            }
+            return true;
+        }
         private void Confirmar_Clicked(object sender, EventArgs e)
         {
-            DisplayAlert("Registro",
-                MiEstudiante.Nombre + " Sus datos se ingresaron correctamente", "Aceptar");
+            if(IsValid() == true)
+            {
+                var mensaje = $"{MiEstudiante.Nombre} sus datos se ingresaron correctamente";
+                string titulo = "Registro";
+                DisplayAlert(titulo, mensaje, "ok");
+            }
+
+
         }
     }
 }
